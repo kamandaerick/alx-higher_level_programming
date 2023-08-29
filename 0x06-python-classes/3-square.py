4faf7ce865f88bf2c88264b8fada52f4be51dfd8
Erick Kamanda <erickckamanda@gmail.com>
2023-08-29 14:13:31 -0700

    Solution to task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Definition os a class and its private instance"""
+
+
+class Square:
+    """The definition of a class called Square"""
+    def __init__(self, size=0):
+        """
+        Constructor.
+        Args:
+            size(int): the length of the sides of the square
+        Raises:
+            TypeError: the value of size is not an integer
+            ValueError: the value of size is a negativie
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        This method calculates the area of a given square object.
+        Return: returns the area of the square
+        """
+        return self.__size * self.__size
