29148e97e47044c13f3161ea7dfde08268c53797
Erick Kamanda <erickckamanda@gmail.com>
2023-08-29 13:46:23 -0700

    Solution to task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Definition os a class and its private instance"""
+
+
+class Square:
+    """The definition of a class called Square"""
+    def __init__(self, size=0):
+        """
+        Constructor.
+        Args:
+            size(int): the length of the sides of the square
+        """
+        try:
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+        except TypeError as e:
+            print(e)
+        except ValueError as e:
+            print(e)
