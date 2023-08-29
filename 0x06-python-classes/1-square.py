bceacff08c80bfc26384ad225dd76fb4991b52ed
Erick Kamanda <erickckamanda@gmail.com>
2023-08-29 12:52:39 -0700

    Solution to task 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+""" This is a shebang the runs python script using python3"""
+
+
+class Square:
+    """
+    This is the definition of a class Square that has one instance called size
+    """
+    def __init__(self, size):
+        """This is an object/instance of class Square"""
+        self.__size = size
