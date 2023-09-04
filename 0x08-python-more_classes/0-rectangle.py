539d59e07f89a6d1e3c24f05b0bf8674025cb60d
Erick Kamanda <erickckamanda@gmail.com>
2023-09-04 12:20:58 -0700

    Solution to task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+    _summary_
+    This proect involves creation of a class Rectangle the define a Rectangle.
+    Importing modules is not allowed at all.__annotations__
+"""
+class Rectangle:
+    """The definition of the class Rectangle"""
+    pass
