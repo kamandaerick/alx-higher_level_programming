a6bfbd361a4451309619ecc1dcb48ac6447d89eb
Erick Kamanda <erickckamanda@gmail.com>
2023-09-04 12:23:08 -0700

    Resolved pycodestyle error by adding 2 blanks lines

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -4,6 +4,8 @@
     This proect involves creation of a class Rectangle the define a Rectangle.
     Importing modules is not allowed at all.__annotations__
 """
+
+
 class Rectangle:
     """The definition of the class Rectangle"""
     pass
