b95df3a31b388a3720054936a075d794f43553c8
Erick Kamanda <info.proresearchers@gmail.com>
2023-09-13 04:53:02 -0700

    Solution to task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Check if an obect is an instance of a class"""
+
+
+def is_same_class(obj, a_class):
+    """Checks if an object is an instance of a class
+
+    Args:
+        obj (object): The object to check
+        a_class (class): The class to check against
+
+    Returns:
+        boolean: returns True if the obect is an instance of a class
+                and False if it is not
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
