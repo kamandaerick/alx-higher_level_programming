a9135c2d5664b17b4e14e057a5011ce3b0bcfc93
Erick Kamanda <info.proresearchers@gmail.com>
2023-09-13 10:37:50 -0700

    Solution to task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define an empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    """An empty class BaseGeometry"""
+    def area(self):
+        """Define a function area
+
+        Raises:
+            Exception: raised since the area is nt defined
+        """
+        raise Exception("area() is not implemented")
