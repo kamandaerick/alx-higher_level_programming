8de76af7b358919bd70991af3907ae21d4587e0c
Erick Kamanda <info.proresearchers@gmail.com>
2023-09-13 05:39:30 -0700

    Solution to task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Check if an obect is an instance of a class
+that inherited (directly or indirectly) from the specified class ;
+otherwise False.
+"""
+
+
+def inherits_from(obj, a_class):
+    """Checks if an object is an instance of a class
+
+    Args:
+        obj (object): The object to check
+        a_class (class): The class to check against
+
+    Returns:
+        boolean: returns True if the obect is an instance of a class
+                and False if it is not
+    """
+    if isinstance(obj, a_class) and not isinstance(type(obj), a_class):
+        return True
+    else:
+        return False
