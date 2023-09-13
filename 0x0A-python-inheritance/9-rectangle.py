5cd888d6425d98183eeecc62f8b7e61d45e4ec68
Erick Kamanda <info.proresearchers@gmail.com>
2023-09-13 12:12:40 -0700

    Solution to task 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Import BaseGeometry class from 7-base_geometry' module"""
+"""Import BaseGeometry class from '7-base_geometry' module"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
@@ -17,3 +17,15 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
+
+    def area(self):
+        """Calculates the area of a triangle
+
+        Returns:
+            int: area
+        """
+        return (self.__height) * (self.__width)
+
+    def __str__(self):
+        """Definition of the __str__ method"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
