384fe3137a947dd1e41c015c1f2547359c60f788
Erick Kamanda <info.proresearchers@gmail.com>
2023-09-21 23:37:01 -0700

    Solutions to task 15 and 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Define a Base class "BAse" """
+import json
 
 
 class Base:
@@ -17,3 +18,32 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return JSON representation of list_dictionaries
+
+        Args:
+            list_dictionaries (list): list of dictionaries
+
+        Returns:
+            JSON: json representation of disctionaries
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write JSON representation of list_objs to a file
+
+        Args:
+            list_objs (object): list of objects inheriting from base
+        """
+        if list_objs is None:
+            json.loads(cls.json, "[]")
+        else:
+            with open(cls.__name__ + ".json", "w") as file:
+                json_data = [obj.to_dictionary() for obj in list_objs]
+                file.write(cls.to_json_string(json_data))
