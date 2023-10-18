cfc42de73e0840ba0c8ae024bbe2831717876d90
Kamanda <info.proresearchers@gmail.com>
2023-10-18 12:50:21 -0700

    Revisions

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,3 @@
--- insert a new rowin the table first_tabl
-USE hbtn_0c_0;
-INSERT INTO first_table (id, name)
+-- insert a new rowin the table first_table
+INSERT INTO hbtn_0c_0.first_table (id, name)
 VALUES (89, 'Best School');
