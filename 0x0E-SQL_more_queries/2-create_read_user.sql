5e3faf6e5d0af745c79d3cf0ec2aad64ee383977
Kamanda <info.proresearchers@gmail.com>
2023-10-20 03:25:24 -0700

    Revised task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,6 @@
 -- Create a databse hbtn_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Create a user user_0d_2
-CREATE USER IF NOT EXISTS'user_0d_2'@'hbtn_0d_2' IDENTIFIED BY 'user_0d_2_pwd'
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- Grant the SELECT priviledge
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
