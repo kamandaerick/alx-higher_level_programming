1881611ade9a6486fae187e37cf276038cb6edbd
Kamanda <info.proresearchers@gmail.com>
2023-10-20 03:17:07 -0700

    Create database hbtn_0d_2 and the user user_0d_2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- Create a databse hbtn_0d_2
-CREATE DATABASE hbtn_0d_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Create a user user_0d_2
 CREATE USER IF NOT EXISTS'user_0d_2'@'hbtn_0d_2' IDENTIFIED BY 'user_0d_2_pwd'
