7511b0572026eff7e7e88b729fc7d2fe7b347604
Kamanda <info.proresearchers@gmail.com>
2023-10-20 02:41:39 -0700

    Show priviledges for users

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- List the priviledges of MySQL user user_0d_1
+-- and user_0d_2 on your server (in localhost).
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Priviledges for the second user
+SHOW GRANTS FOR 'user_0d_2'@'localhost'
