78f0dfe446557009182542d075c819cf91d2f6e6
Kamanda <info.proresearchers@gmail.com>
2023-10-20 08:53:43 -0700

    TAsk 9

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,8 @@
 -- List cities of California
-SELECT id, name
-FROM cities
-WHERE cities.state_id IN (SELECT id FROM states WHERE name = 'California');
-ORDER BY id ASC;
+SELECT id, name FROM cities
+WHERE state_id IN
+(
+	SELECT id FROM states
+	WHERE name = 'California'
+)
+ORDER BY id;
