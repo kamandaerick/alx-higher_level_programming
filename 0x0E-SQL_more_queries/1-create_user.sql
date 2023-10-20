3cc2044618a6d0d568e8a1240ba953833d8f902b
Kamanda <info.proresearchers@gmail.com>
2023-10-20 03:05:29 -0700

    Grant priviledges to the user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,3 @@
 -- Create a user user_0d_1
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd'
-iGRANT ALL PRIVILEGES ON *. * TO 'user_0d_1'@'%';
-FLUSH PRIVILEGES;
-
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
