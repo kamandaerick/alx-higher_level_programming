b5065ea94bbcd9761ad159341fb8b9a0f1e51987
Kamanda <erickckamanda@gmail.com>
2023-11-13 17:27:14 +0000

    Print 3 lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -3,9 +3,4 @@
  * This script prints 3 lines using 3 different
  * logs to the console
  */
-// first log
-console.log('C is fun');
-// second log
-console.log('Python is cool');
-// third log
-console.log('JavaScript is amazing');
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
