db33aeed97842d6abfd845116ad4d864c6d066e0
Kamanda <erickckamanda@gmail.com>
2023-11-13 17:13:02 +0000

    Print 3 lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,4 +2,6 @@
 /**
  * This script prints 3 lines
  */
-console.log('C is fun\nPython is cool\nJavaScript is Amazing\n');
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is Amazing');
