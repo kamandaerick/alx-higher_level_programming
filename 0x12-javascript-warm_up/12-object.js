5f0e9a29f5565299285c6732cd250794e878901d
Kamanda <erickckamanda@gmail.com>
2023-11-14 02:30:22 +0000

    Update the value of an object

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/**
+ * This script defines and updates an object
+ */
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+// update the value of the object to 89
+myObject.value = 89;
+console.log(myObject);
