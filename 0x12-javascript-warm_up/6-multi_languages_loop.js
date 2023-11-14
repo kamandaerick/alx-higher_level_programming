87088b8a1c91469c8749025229b4f1b0ee5154af
Kamanda <erickckamanda@gmail.com>
2023-11-14 01:11:08 +0000

    Print array elements each on a new line

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/**
+ * Print the elements of an array each on a new line
+ */
+const sentences = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < 3; i++) {
+  console.log(sentences[i]);
+}
