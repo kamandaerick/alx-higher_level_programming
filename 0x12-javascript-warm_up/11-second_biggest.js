37d2f47afc5325cde68d4fb624badb192761d323
Erick Kamanda <info.proresearchers@gmail.com>
2023-11-15 15:17:19 +0300

    Corrected documentation

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,20 +2,13 @@
 /**
  * This script prints the second largest number in an array
  */
-const arr = [];
-const len = process.argv.length;
-
-if (len === 2 || len === 3) {
+// Check if there are command line arguments
+if (process.argv.length <= 2) {
+  console.log(0);
+} else if (process.argv.length === 3) {
   console.log(0);
 } else {
-  for (let i = 2; i < len; i++) {
-    arr.push(process.argv[i]);
-  }
-  /**
-   * Sorting function for numbers in the array
-   */
-  arr.sort(function (a, b) {
-    return a - b;
-  });
-  console.log(arr[arr.length - 2]);
+  const numbers = process.argv.slice(2).map(arg => parseInt(arg));
+  const sortedNumbers = numbers.sort((a, b) => b - a);
+  console.log(sortedNumbers[1]);
 }
