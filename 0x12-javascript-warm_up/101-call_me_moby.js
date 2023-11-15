15a02e16f13758db7ea7305c02ee0017bb8edbda
Kamanda <erickckamanda@gmail.com>
2023-11-15 14:47:35 +0000

    Execute a function x-times

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,13 +1,15 @@
 #!/usr/bin/node
 /**
- * This script executes a function x times
+ * This script executes a function a number of times
  * @param {int} x the number of times to call the function
  * @param {Function} theFunction the function to call
  */
 function myFunc (x, theFunction) {
-  for (let i = 0; i < x; i++) {
-    theFunction();
+  if (x <= 0) {
+    return;
   }
+  theFunction();
+  myFunc(x - 1, theFunction);
 }
 module.exports = {
   myFunc
