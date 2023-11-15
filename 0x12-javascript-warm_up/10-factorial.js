6be0a941a485be8a76a92ce21622ee57f9684fa9
Kamanda <erickckamanda@gmail.com>
2023-11-15 08:39:47 +0000

    Fix semistandard documentation errors

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,21 +1,21 @@
 #!/usr/bin/node
 /**
- * This script takes the first commandline argument and
- * checks if it is an int then finds its factorial
+ *This script takes the first commandline argument and
+ *checks if it is an int then finds its factorial
  */
-let number = Number(process.argv[2]);
+const number = Number(process.argv[2]);
 /**
- * 
+ *This function finds a factorial of a number
  * @param {int} number the number entered as commandline arg
  * @returns an integer (the result of a factorial)
  */
-function factorial(number) {
+function factorial (number) {
   if (isNaN(number) || number % 1 !== 0) {
     return 1;
   } else if (number === 0 || number === 1) {
     return 1;
   } else {
-    let f = number * factorial(number - 1);
+    const f = number * factorial(number - 1);
     return f;
   }
 }
