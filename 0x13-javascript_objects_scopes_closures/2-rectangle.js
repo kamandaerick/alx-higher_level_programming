b141013be7c8ad8b05b76367abefb5ea256bb736
Kamanda <erickckamanda@gmail.com>
2023-11-17 21:12:55 +0000

    Triangle

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+/**
+ * Define and export a class Rectangle with constructor taking 2 arguments
+ */
+class Rectangle {
+  /**
+   * Constructor
+   * @param {int} w width of a rectangle
+   * @param {int} h height of a rectangle
+   */
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      return {};
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
