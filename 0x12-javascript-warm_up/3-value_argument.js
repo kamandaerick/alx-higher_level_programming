0a963d536c8e309048cbefc31c1cfeb6eeb13f89
Kamanda <erickckamanda@gmail.com>
2023-12-11 16:44:27 +0000

    Print the first commandline argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,11 +2,8 @@
 /**
  * This script prits the first argument passed to it
  */
-const allArguments = process.argv;
-if (allArguments.length <= 2) {
+if (!process.argv[2]) {
   console.log('No argument');
 } else {
-  for (let i = 2; i < allArguments.length; i++) {
-    console.log(process.argv[i]);
-  }
+  console.log(process.argv[2]);
 }
