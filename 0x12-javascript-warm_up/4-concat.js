5b24053b72526abee2346d7260d83523bd579eb0
Kamanda <erickckamanda@gmail.com>
2023-12-11 16:52:45 +0000

    Corected the formating of the output

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,4 +3,4 @@
  * This script prints commandline args in this format
  *first argument "is" second argument
  */
-console.log(process.argv[2] + ' is' + process.argv[3]);
+console.log(process.argv[2] + ' is ' + process.argv[3]);
