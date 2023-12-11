37c7075a9e0a6b22cf25708891af9f3e5c7ae6ea
Kamanda <erickckamanda@gmail.com>
2023-12-11 18:30:30 +0000

    Function inrements and calls a function

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -3,10 +3,10 @@
  * @param {int} number the number to update
  * @param {Function} theFunction the function to call
  */
-function myFunc (number, theFunction) {
+function addMeMaybe (number, theFunction) {
   number += 1;
-  theFunction();
+  theFunction(number);
 }
 module.exports = {
-  myFunc
+  addMeMaybe
 };
