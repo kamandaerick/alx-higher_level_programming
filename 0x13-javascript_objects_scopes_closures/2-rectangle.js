86c96a2f36b55228a1dedc4f65f4509e742c5026
Kamanda <erickckamanda@gmail.com>
2023-12-12 22:50:49 +0000

    Check if the rectangle parameters are positive integers

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/**
+ * Define and export a class Rectangle with constructor taking 2 arguments
+ */
+class Rectangle {
+  /**
+   * Check the values of h and w if positive
+   * @param {int} w width of a rectangle
+   * @param {int} h height of a rectangle
+   */
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) return;
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
