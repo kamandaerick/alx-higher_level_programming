fc622fec0c4fac4ad0338f6eb096ab40062d6721
Kamanda <erickckamanda@gmail.com>
2023-12-21 06:52:47 -0800

    List all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,7 +11,13 @@ username = sys.argv[1]
 password = sys.argv[2]
 db_name = sys.argv[3]
 
-db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=db_name)
+db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+        )
 c = db.cursor()
 
 querry = "SELECT * FROM states ORDER BY states.id"
@@ -23,9 +29,7 @@ states = c.fetchall()
 for state in states:
     state_id = state[0]
     state_name = state[1]
-    
     print(f"({state_id}, '{state_name}')")
 
 c.close()
 db.close()
-
