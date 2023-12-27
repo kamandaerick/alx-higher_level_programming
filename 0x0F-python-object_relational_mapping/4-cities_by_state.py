39377bdb6966a56f8a38407188c2323ab8617109
Kamanda <erickckamanda@gmail.com>
2023-12-27 07:03:50 -0800

    Revision

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,46 +1,30 @@
 #!/usr/bin/python3
-import MySQLdb
+"""
+Lists all cities from the database hbtn_0e_4_usa,
+ordered by city id.
+"""
 import sys
-""" This script uses JOIN to list cities in every state"""
-
-
-def list_cities_by_state(username, password, db_name):
-    """Function to handle to connect to the datase and handle the query
-
-    Args:
-        username (string): username
-        password (string): password
-        db_name (string): database name
-    """
-    try:
-        db = MySQLdb.connect(
-            host='localhost',
-            port=3306,
-            user=username,
-            passwd=password,
-            db=db_name
-        )
-        cursor = db.cursor()
-        query = (
-            "SELECT cities.id, cities.name, states.name "
-            "FROM cities "
-            "JOIN states ON cities.state_id = states.id "
-            "ORDER BY cities.id ASC"
-        )
-        cursor.execute(query)
-        cities = cursor.fetchall()
-        for city in cities:
-            city_id, city_name, state_name = city[0], city[1], city[2]
-            print(f"({city_id}, '{city_name}', '{state_name}')")
-
-    except MySQLdb.Error as e:
-        print("Error:", e)
-    finally:
-        if 'db' in locals() and db.open:
-            cursor.close()
-            db.close()
-
+import MySQLdb
 
 if __name__ == "__main__":
-    username, password, db_name = sys.argv[1:4]
-    list_cities_by_state(username, password, db_name)
+    if len(sys.argv) != 4:
+        sys.exit(1)
+    db = MySQLdb.connect(
+      host="localhost",
+      port=3306,
+      user=sys.argv[1],
+      passwd=sys.argv[2],
+      db=sys.argv[3]
+      )
+    
+    c = db.cursor()
+
+    c.execute("SELECT `cities`.`id`, `cities`.`name`, `states`.`name` \
+               FROM `cities` \
+               INNER JOIN `states` ON `cities`.`state_id` = `states`.`id` \
+               ORDER BY `cities`.`id`")
+
+    [print(city) for city in c.fetchall()]
+
+    c.close()
+    db.close()
