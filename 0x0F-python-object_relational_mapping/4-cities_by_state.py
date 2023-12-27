8e4e759bd907727afa452fe5b2f16447765b5af7
Kamanda <erickckamanda@gmail.com>
2023-12-27 07:06:17 -0800

    Correction

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -16,7 +16,6 @@ if __name__ == "__main__":
       passwd=sys.argv[2],
       db=sys.argv[3]
       )
-    
     c = db.cursor()
 
     c.execute("SELECT `cities`.`id`, `cities`.`name`, `states`.`name` \
