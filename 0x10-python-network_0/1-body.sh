08167d5f06d6904529377917ae48588fa32aaeb2
Kamanda <erickckamanda@gmail.com>
2024-01-05 09:36:30 -0800

    Display the body if status code is 200

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-#display the body of http get response
-url=$1
-
-response=$(curl -sIX GET "$url" | head -n 1 | awk '{print $2}')
-content=$(curl -sX GET "$url")
-
-if [ -n "$response" ] && [ "$response" -eq 200 ];
-then
-	echo "$content"
-else
-	echo "An error occured"
-	exit 1
-fi
+# Get the response body for a given URL for 200 status code responses.
+curl -sL "$1"
