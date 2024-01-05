885f11fc8d496da0339feabc667fd701edff48a0
Kamanda <erickckamanda@gmail.com>
2024-01-05 10:53:46 -0800

    Send a POST request to the passed url and display body

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #send a POST request to the passed URL, and display the body of the response
-curl -sX POST "email=test@gmail.com&subect=I will always be here for PLD" "$1" 
+curl -sX POST -H "Content-Type: application/json" -d '{"email": "test@gmail.com", "subject": "I will always be here for PLD"}' "$1"
