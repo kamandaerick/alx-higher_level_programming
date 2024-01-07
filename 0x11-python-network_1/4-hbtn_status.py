083b574846b0ac0ff17f6abae93849b536c121bf
Kamanda <erickckamanda@gmail.com>
2024-01-07 15:28:24 -0800

    fetch the given url using requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""
+ fetches https://alx-intranet.hbtn.io/status
+"""
+
 import requests
-"""Fetch the given url using requests"""
 
-
-if __name__ == "__main__":
-    r = requests.get("https://alx-intranet.hbtn.io/status")
+if __name__ == '__main__':
+    r = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
-    print(f"\t - type: {type(r.text)}")
-    print(f"\t - content: {r.text}")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
