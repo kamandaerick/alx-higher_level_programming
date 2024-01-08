b1351e20162dfac4928a4eb74745e9acd8fda81b
Kamanda <erickckamanda@gmail.com>
2024-01-07 16:40:09 -0800

    Handle error

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Send a request to given url and display the body"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    r = requests.get(url)
+    if r.status_code > 400:
+        print("Error code: ", r.statuscode)
+    else:
+        print(r.text)
