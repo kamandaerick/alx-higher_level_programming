893fba36daed68e96b735e751a0f20a7cc3d81de
Kamanda <erickckamanda@gmail.com>
2024-01-07 16:21:53 -0800

    POST request with a parameter

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,4 +1,4 @@
-#!/usr/in/python3
+#!/usr/biin/python3
 """Take in a URL and an email address, send a POST
     request with an email as a parameter and display
     the body
@@ -9,7 +9,7 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    parameter = {"email": sys.arggv[2]}
+    parameter = {"email": sys.argv[2]}
 
     r = requests.post(url, data=parameter)
     print(r.text)
