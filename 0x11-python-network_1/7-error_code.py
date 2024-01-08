f2eec8979dc8bfb0c8cb5ab3e48d4528e790923d
Kamanda <erickckamanda@gmail.com>
2024-01-07 16:46:16 -0800

    Fixed the string format method

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
     r = requests.get(url)
     if r.status_code >= 400:
-        print("Error code: ", r.statuscode)
+        print("Error code: {}".format(r.statuscode))
     else:
         print(r.text)
