9f3a657a3cff665b1c2fab4760ea2b23c276c68a
Erick Kamanda <117100000+kamandaerick@users.noreply.github.com>
2024-02-10 08:55:35 +0300

    Delete 0x11-python-network_1 directory

--- 0x11-python-network_1/README.md
+++
@@ -1,5 +0,0 @@
-Python - Network #1
-
-This project involve tackling technical questions on Networks in python using the urlli module.
--urllib.request
--urllib.parse
--- 0x11-python-network_1/t.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import sys
-import urllib.request
-
-if __name__ == "__main__":
-    url = sys.argv[1]
-    with urllib.request.urlopen(url) as response:
-        X_id = dict(response.headers)
-        print(X_id.get("X-Request-Id"))
