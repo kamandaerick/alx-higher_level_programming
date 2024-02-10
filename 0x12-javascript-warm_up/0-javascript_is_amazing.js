c54c9b26df8fbf7a1bf046acbaa42a75d45eff88
Erick Kamanda <117100000+kamandaerick@users.noreply.github.com>
2024-02-10 09:01:24 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-/**
- * This script demonstrates a simple node.js script
- * It defines a variable, assigns it a value and prints it
- */
-// variable declaration with a string value
-const myVar = 'JavaScript is amazing';
-// log the variable to the console
-console.log(myVar);
--- 0x12-javascript-warm_up/1-multi_languages.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-/**
- * This script prints 3 lines
- */
-console.log('C is fun\nPython is cool\nJavaScript is amazing');
