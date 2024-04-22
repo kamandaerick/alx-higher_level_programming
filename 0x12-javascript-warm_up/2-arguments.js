9ed04579fca4eb47741e74c13f4dd11655235a33
Kamanda <erickckamanda@gmail.com>
2024-04-22 10:58:07 -0700

    Commandline arguments in javascript

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/**
+ * This script checks for the number of commandline arguments
+ * and prints the appropriate message
+ */
+
+const allArguments = process.argv;
+
+if (allArguments.length === 2) {
+  console.log('No argument');
+} else if (allArguments.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
