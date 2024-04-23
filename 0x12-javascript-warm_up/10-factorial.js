405a6fe64fbe90f91734b00c5f43e8f954180414
Kamanda <erickckamanda@gmail.com>
2024-04-23 02:07:33 -0700

    Factorial using recursion

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,17 @@
 #!/usr/bin/node
 
 const n = parseInt(process.argv[2]);
-let factorial = 1;
 
-if (isNaN(n)) factorial = 1;
-
-for (let i = 1; i <= n; i++) {
-  factorial = factorial * i;
+function factorial (a) {
+  if (a === 0) {
+    return 1;
+  } else {
+    return a * factorial(a - 1);
+  }
 }
-console.log(factorial);
+if (isNaN(n)) {
+  console.log(1);
+} else {
+  const result = factorial(n);
+  console.log(result);
+}
