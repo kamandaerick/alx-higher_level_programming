56e59ae6ec5aea602ceb7b9721c0c80e8cc653f3
Kamanda <erickckamanda@gmail.com>
2024-04-23 10:50:31 -0700

    Check of the dimensions are greater than zero

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,13 +2,13 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
       this.emptyObject = {};
     } else {
       this.width = w;
       this.height = h;
     }
   }
-
 }
+
 module.exports = Rectangle;
