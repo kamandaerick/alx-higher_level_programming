8f6909356360d1a883a92b9d54080595ee78d20b
Kamanda <erickckamanda@gmail.com>
2024-04-24 23:31:01 -0700

    Check for dimensions

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,13 @@
+#!/usr/bin/node
+
 class Rectangle {
-  constructor(w, h) {
-    if (w > 0 && h > 0) {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      return {};
+    } else {
       this.width = w;
       this.height = h;
-    } else {
-      return {};
     }
   }
 }
-
 module.exports = Rectangle;
