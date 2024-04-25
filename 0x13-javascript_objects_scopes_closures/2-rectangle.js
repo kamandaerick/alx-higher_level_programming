2b922e85500cb6ed01f7bb5a9e0f16237294fa30
Kamanda <erickckamanda@gmail.com>
2024-04-24 23:41:53 -0700

    Correcton task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,9 +2,7 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      return {};
-    } else {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
       this.width = w;
       this.height = h;
     }
