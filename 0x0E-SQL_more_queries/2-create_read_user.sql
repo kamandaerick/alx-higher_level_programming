3dfd936658e0e269f65d6e376cf73acd5628b85e
Kamanda <erickckamanda@gmail.com>
2024-05-22 14:48:28 -0700

    TAsk 2 Revsied

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,6 @@
 -- create a user user_0d_2 with SELECT priviledges
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- create a user user_0d_2 with SELECT priviledges
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0c_0.* TO 'user_0d_2'@'localhost';
-FLUSH PRIVILEGES;
+-- grant SELECT on hbtn_0c_0 to user_0d_2
+GRANT SELECT ON hbtn_0c_2.* TO 'user_0d_2'@'localhost';
