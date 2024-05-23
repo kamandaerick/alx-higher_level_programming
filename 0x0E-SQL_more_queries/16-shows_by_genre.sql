e9b85a3daf37937db2d8ef43775330889a012c66
Kamanda <erickckamanda@gmail.com>
2024-05-23 10:05:51 -0700

    Revised

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
--- list all shows and their genres
+-- List all shows and their corresponding genres
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
-JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title ASC, tv_genres.name ASC;
