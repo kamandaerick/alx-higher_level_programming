d467e9211e53ec2cfbea5baea8047fbfd3871a5f
Kamanda <erickckamanda@gmail.com>
2024-05-23 09:40:16 -0700

    Genres for the show Dexter

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,6 @@
 -- list genres of the show Dexter
-SELECT tv_genres.name  as name
+SELECT tv_genres.name
 FROM tv_genres
-LEFT JOIN tv_shows ON tv_shows.id = tv_genres.id
-WHERE tv_shows.title = 'Dexter'
-ORDER BY name;
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter';
