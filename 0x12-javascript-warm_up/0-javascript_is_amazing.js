85ea8950f37e6fa14b565fce8ec741bb538b4bf5
Kamanda <erickckamanda@gmail.com>
2024-06-17 11:47:41 -0700

    Print a variable

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/**
+ * This script demonstrates a simple node.js script
+ * It defines a variable, assigns it a value and prints it
+ */
+// variable declaration with a string value
+const myVar = 'JavaScript is amazing';
+// log the variable to the console
+console.log(myVar);
