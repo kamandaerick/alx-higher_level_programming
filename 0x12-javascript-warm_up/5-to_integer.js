9a9854f6897b01d4abae4182c648e696f38975c6
Kamanda <erickckamanda@gmail.com>
2024-06-17 13:31:31 -0700

    Check if argument can be converted to an int

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/**
+ * This script checks if the first cmdline argument
+ * can be converted to an integer and converts it
+ */
+const input = process.argv[2];
+
+const convertedNumber = Number(input);
+
+if (!isNaN(convertedNumber) && Number.isInteger(convertedNumber)) {
+  console.log(`My number: ${convertedNumber}`);
+} else {
+  console.log('Not a number');
+}
+
