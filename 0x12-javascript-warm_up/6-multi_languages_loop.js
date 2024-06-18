9eaf0e0097daf731dea19b2440a67c7da441e5e0
Kamanda <erickckamanda@gmail.com>
2024-06-18 03:14:29 -0700

    Iterate an array using a for loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/**
+ * This script iterates and prints items of an array
+ * using a for in loop
+ */
+let arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i in arr) {
+    console.log(arr[i]);
+}
