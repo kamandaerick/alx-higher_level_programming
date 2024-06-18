c7b7e9e6a5f99d1f23f0bbf179803bf548fd8518
Kamanda <erickckamanda@gmail.com>
2024-06-18 06:10:55 -0700

    Revision

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,17 +1,14 @@
 #!/usr/bin/node
 /**
- * This script prints the second largest number in an array
+ * Find the second largest number in a list of args
  */
-for (let i = 2; i < process.argv.length; i++) {
-  process.argv[i] = Number(process.argv[i]);
+const a = process.argv;
+const args = [];
+for (let i = 2; i < a.length; i++) {
+  args.push(a[i]);
 }
-
-if (process.argv.length <= 2) {
-  console.log(0);
-} else if (process.argv.length === 3) {
+if (args.length <= 1) {
   console.log(0);
 } else {
-  const numbers = process.argv.slice(2).map(arg => parseInt(arg));
-  const sortedNumbers = numbers.sort((a, b) => b - a);
-  console.log(sortedNumbers[1]);
+  console.log(parseInt(args.sort().reverse()[1]));
 }
