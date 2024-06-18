7f7da709cc4a675e12c9c482f43ab0336e0133b0
Kamanda <erickckamanda@gmail.com>
2024-06-18 03:17:56 -0700

    Fix documentation error

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,7 +3,7 @@
  * This script iterates and prints items of an array
  * using a for in loop
  */
-let arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i in arr) {
-    console.log(arr[i]);
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const i in arr) {
+  console.log(arr[i]);
 }
