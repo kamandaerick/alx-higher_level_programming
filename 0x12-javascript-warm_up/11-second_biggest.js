5c09b873803bd281235d157d7efac93b4554a27d
Kamanda <erickckamanda@gmail.com>
2024-06-18 06:01:50 -0700

    Print the second largest integer

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/**
+ * Find the second largest number in a list of args
+ */
+const a = process.argv;
+const args = [];
+for (let i = 2; i < a.length; i++) {
+  args.push(a[i]);
+}
+if (args.length <= 1) {
+  console.log(0);
+} else {
+  console.log(args.sort().reverse()[1]);
+}
