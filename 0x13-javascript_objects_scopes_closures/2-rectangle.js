e6943ddf614631bf3c3ef9ad33e3117203a2e8f1
Kamanda <erickckamanda@gmail.com>
2024-06-19 02:51:21 -0700

    Validate the dimensions

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/**
+ * Check the dimensions of the rectangle
+ */
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      this.emptyObject = {};
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
