1cbc2e31ba08e7da7a63ed4320bb0a7cd5cf8370
Kamanda <erickckamanda@gmail.com>
2024-06-22 10:27:40 -0700

    Revise class State

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,12 +5,14 @@ from sqlalchemy.ext.declarative import declarative_base
 import sys
 
 Base = declarative_base()
-
+'''Define class State inheriting from BAse'''
 class State(Base):
+    '''
+    __tablename__ (str): Name of the table
+    id (int): Primary key (state id)
+    name (str): Name of the state
+    '''
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
