7e78ff015616cae930565dd7d2cfc60c6d563afe
Kamanda <erickckamanda@gmail.com>
2024-06-22 02:34:40 -0700

    Throw error if more or less than 4 arguments are passed

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,26 +4,42 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python script.py <username> <password> <database>")
-        sys.exit(1)
+    # Get command line arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    db_connection = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-    cursor = db_connection.cursor()
-    query = "SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-    cursor.execute(query)
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
+    try:
+        # Establish a connection to the database
+        db_connection = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
 
-    cursor.close()
-    db_connection.close()
+        # Create a cursor object
+        cursor = db_connection.cursor()
+
+        # Execute an SQL query to select states with names starting with 'N' in ascending order by id
+        query = "SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+        cursor.execute(query)
+
+        # Fetch all the results
+        rows = cursor.fetchall()
+
+        # Process and print the results
+        for row in rows:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print(f"Error connecting to MySQL Platform: {e}")
+
+    finally:
+        # Ensure resources are closed properly
+        if cursor:
+            cursor.close()
+        if db_connection:
+            db_connection.close()
+
