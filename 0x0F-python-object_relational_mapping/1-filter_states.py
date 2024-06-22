005027a1667c6160013abfe3360605d5aa03acf7
Kamanda <erickckamanda@gmail.com>
2024-06-22 02:30:49 -0700

    Throw error if more or less than 4 arguments are passed

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,7 +4,9 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <database>")
+        sys.exit(1)
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
