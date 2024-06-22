bd8fcfe5a326edca12217903f7ef1dacec6aeb55
Kamanda <erickckamanda@gmail.com>
2024-06-22 02:51:04 -0700

    Fix error

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
-"""List states whose name starts with N from the database hbtn_0e_0_usa"""
-import sys
-import MySQLdb
+"""
+This script lists all states with
+a `name` starting with the letter `N`
+from the database `hbtn_0e_0_usa`.
+"""
 
-if __name__ == "__main__":
-    if len(sys.argv) == 4:
-        db = MySQLdb.connect(host="localhost",
-                             port=3306,
-                             user=sys.argv[1],
-                             passwd=sys.argv[2],
-                             db=sys.argv[3])
-        cur = db.cursor()
-        cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
-        rows = cur.fetchall()
-        for row in rows:
-            print(row)
-        cur.close()
-        db.close()
-    else:
-        exit(1)
+import MySQLdb as db
+from sys import argv
 
+"""
+Access to the database and get the states
+from the database.
+"""
+
+if __name__ == '__main__':
+    db_connect = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
+    db_cursor = db_connect.cursor()
+
+    db_cursor.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' \
+                ORDER BY states.id ASC")
+
+    rows_selected = db_cursor.fetchall()
+
+    for row in rows_selected:
+        print(row)
