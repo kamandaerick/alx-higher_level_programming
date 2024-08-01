a428618bcdcc771c3139f56414c78813510d4571
Kamanda <erickckamanda@gmail.com>
2024-08-01 01:30:48 -0700

    Correct identation warnings

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,9 +5,9 @@ const fs = require('fs');
 const file = process.argv[2];
 
 fs.readFile(file, 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(data);
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
