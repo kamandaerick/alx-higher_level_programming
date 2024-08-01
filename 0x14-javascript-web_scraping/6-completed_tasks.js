f75c004cbd51709976b4352540335a496b6a10c6
Kamanda <erickckamanda@gmail.com>
2024-08-01 04:18:20 -0700

    Number of tasks completed

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const completed = {};
+    const tasks = JSON.parse(body);
+    for (const task of tasks) {
+      if (task.completed) {
+        if (completed[task.userId]) {
+          completed[task.userId] += 1;
+        } else {
+          completed[task.userId] = 1;
+        }
+      }
+    }
+    console.log(completed);
+  }
+});
