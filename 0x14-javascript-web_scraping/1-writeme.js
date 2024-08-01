54d2e77e83899866ae3d29b28799585adc16f0d1
Kamanda <erickckamanda@gmail.com>
2024-08-01 02:04:43 -0700

    Write to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const fileName = process.argv[2];
+
+const content = process.argv[3];
+
+fs.writeFile(fileName, content, (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
